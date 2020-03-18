e9add53500bb6b277f7c4b42b408dcf52b2ca5b7
adisaw <46715774+adisaw@users.noreply.github.com>
2020-03-18 23:08:25 +0530

    Add files via upload

---
+++ Assignment3/ass3_17CS10060_test.c
@@ -0,0 +1,105 @@
+
+
+//ADITYA SAWANT 17CS10060
+#define test "this d will test"
+
+
+extern cout {
+    	int jk= func();
+}
+
+void func(){
+	union a;
+	h=a-b ;
+}
+
+void inline test(char ch[], int *restrict )
+{
+    	printf("using keyword inline \n");
+}
+
+typedef struct node {
+    	struct node* next;
+    	volatile signed short d = 5;
+
+}node;
+section:
+int main() 
+{
+	
+	static auto a = -5;
+	register float b = 4.0E5;
+	double val = -40.9;
+	unsigned long c = 23;
+	char sd='\n';
+
+	_Bool boolean;
+    	_Complex complex;
+	_Imaginary imag;
+	...
+	node* root = NULL;
+	root->next = NULL;
+	root->d = 5 + 1 - 1 / 1 * 1;
+
+	/* test command 
+		multiline
+	*/
+	while( int i=6; i<=9;i++)
+	{
+		sum=sum+3;
+    	}
+	
+    	int j = 1;
+    	char ch[] = "hi";
+    
+    	//float c=9.00;
+    	float value=.00032;
+    	do{
+        	if(a>=b)
+        	{
+        	    j%=2;
+        	    j>>=2;
+        	    j<<=2;
+        	    j&=0;
+        	    j^=2;
+        	    j|=23;
+        	}
+        	else if(a<=b)
+        	{
+        	    if(a>b);
+        	    if(a<b);
+        	    if(a==b);
+        	    break;
+        	}
+
+    	}while(1);
+    	enum enums{h,k,l};
+    	j = sizeof('h');
+
+	const float a=90.0;
+    	for(int i=0;i<5;i++) 
+	{
+        	switch(i) {
+            	case(1) : continue;
+            	case(2) : break;
+            	default : return 0;
+        	}
+    	}
+    	goto section;
+	for(int i = 10; i>4; i--)
+   	{
+   	     e+=1;
+   	     e*=2;
+   	     e-=2;
+   	     e/=2;
+   	     e--;
+
+     	  	if(1 && 0 || !1 & !=9)
+		{        
+		    	j = ~j;
+		}
+	}
+    	return 0;
+}
+
+    
